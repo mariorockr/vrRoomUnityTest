68f716c8685bc7921a2add65c4457f4401ef7ac9
Mario Ramirez <mario@mozper.com>
2023-04-12 13:00:50 -0600

    Se agrega timer ejemplo

--- Assets/TimerObjectScript.cs
+++ Assets/TimerObjectScript.cs
@@ -1,13 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class TimerObjectScript : MonoBehaviour
 {
+    public TextMeshProUGUI text;
+    public float timeRemaining = 10;
+    public bool timerIsRunning = false;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        text.text = "00:00:00";
     }
 
     bool isColliding = false;
@@ -15,9 +21,20 @@ public class TimerObjectScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (isColliding)
+        if (timerIsRunning)
         {
             print("Colisionando");
+            if (timeRemaining > 0)
+            {
+                timeRemaining -= Time.deltaTime;
+                DisplayTime(timeRemaining);
+            }
+            else
+            {
+                Debug.Log("Time has run out!");
+                timeRemaining = 0;
+                timerIsRunning = false;
+            }
         }
         
     }
@@ -25,12 +42,20 @@ public class TimerObjectScript : MonoBehaviour
     {
         if (collision.gameObject.tag == "Pizza") {
             isColliding = true;
+            timerIsRunning = true;
             print("Entro");
         }
        
 
     }
 
+    void DisplayTime(float timeToDisplay)
+    {
+        timeToDisplay += 1;
+        float minutes = Mathf.FloorToInt(timeToDisplay / 60);
+        float seconds = Mathf.FloorToInt(timeToDisplay % 60);
+        text.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
 
     void OnCollisionExit(Collision other)
     {
